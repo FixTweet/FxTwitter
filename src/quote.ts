e1b9e063a302f31bd94c99d10dcc896474729577
dangered wolf <d@ngeredwolf.me>
2022-07-15 16:36:49 -0400

    Improved quote tweet style

--- src/quote.ts
+++ src/quote.ts
@@ -4,9 +4,9 @@ export const handleQuote = (quote: TweetPartial): string | null => {
   console.log('quote tweet: ', quote);
 
   let str = `\n`;
-  str += `↘️ Quoting ${quote.user?.name} (@${quote.user?.screen_name}) `;
+  str += `════ ↘️ Quoting ${quote.user?.name} (@${quote.user?.screen_name}) ════`;
 
-  str += '═'.repeat(Math.max(60 - str.length, 0));
+  // str += '═'.repeat(Math.max(60 - str.length, 0));
 
   str += ` \n\n`;
   str += linkFixer(quote, quote.full_text);
