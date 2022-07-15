41f770e09a3fb0a53b78deeca7a5106ac4bc323f
dangered wolf <d@ngeredwolf.me>
2022-07-14 20:39:40 -0400

    Delete extra format function

--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,6 @@ import { Router } from 'itty-router';
 import { Constants } from './constants';
 import { handleStatus } from './status';
 
-/*
-  Useful little function to format strings for us
-*/
-
-declare global {
-  interface String {
-    format(options: any): string;
-  }
-}
-
-String.prototype.format = function (options: any) {
-  return this.replace(/{([^{}]+)}/g, (match: string, name: string) => {
-    if (options[name] !== undefined) {
-      return options[name];
-    }
-    return match;
-  });
-};
-
 const router = Router();
 
 const statusRequest = async (request: any, event: FetchEvent) => {
