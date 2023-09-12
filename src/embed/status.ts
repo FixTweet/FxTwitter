dabe538f3569f6e4a4c8ca975ce0d9082cad4037
dangered wolf <d@ngeredwolf.me>
2023-09-12 06:15:23 -0400

    Increase maximum authorText to 256

--- src/embed/status.ts
+++ src/embed/status.ts
@@ -371,7 +371,7 @@ export const handleStatus = async (
     `<link rel="alternate" href="{base}/owoembed?text={text}{deprecatedFlag}&status={status}&author={author}&useXbranding={useXBranding}" type="application/json+oembed" title="{name}">`.format(
       {
         base: Constants.HOST_URL,
-        text: encodeURIComponent(truncateWithEllipsis(authorText, 250)),
+        text: encodeURIComponent(truncateWithEllipsis(authorText, 256)),
         deprecatedFlag: flags?.deprecated ? '&deprecated=true' : '',
         status: encodeURIComponent(status),
         author: encodeURIComponent(tweet.author?.screen_name || ''),
