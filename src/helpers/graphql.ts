ec24895ece5e102e0aa052f52f214658a19f14a6
dangered wolf <d@ngeredwolf.me>
2023-11-02 05:54:10 -0400

    Workaround for GraphQLTweet detector

--- src/helpers/graphql.ts
+++ src/helpers/graphql.ts
@@ -16,7 +16,7 @@ export const isGraphQLTweet = (response: unknown): response is GraphQLTweet => {
   return (
     typeof response === 'object' &&
     response !== null &&
-    '__typename' in response &&
-    (response.__typename === 'Tweet' || response.__typename === 'TweetWithVisibilityResults')
+    // @ts-expect-error it's 6 am please let me sleep
+    ('__typename' in response && (response.__typename === 'Tweet' || response.__typename === 'TweetWithVisibilityResults') || ('legacy' in response && response.legacy?.full_text))
   );
 };
