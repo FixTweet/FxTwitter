4261fec1fa1f00731d713cdca7110ba0e0b76afb
dangered wolf <d@ngeredwolf.me>
2023-11-04 03:52:55 -0400

    Fix scenario where truncating on a UTF-16 character breaks

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -15,8 +15,23 @@ export const unescapeText = (text: string) => {
     .replace(/&amp;/g, '&');
 };
 
-export const truncateWithEllipsis = (str: string, maxLength: number): string =>
-  str.length > maxLength ? str.substring(0, maxLength - 1) + '…' : str;
+export const truncateWithEllipsis = (str: string, maxLength: number): string => {
+  const segmenter = new Intl.Segmenter('en', { granularity: 'grapheme' });
+  const segments = segmenter.segment(str);
+  let truncated = '';
+  let length = 0;
+
+  for (const segment of segments) {
+    if (length + segment.segment.length > maxLength) {
+      break;
+    }
+    truncated += segment.segment;
+    length += segment.segment.length;
+  }
+
+  return truncated.length < str.length ? truncated + '…' : truncated;
+};
+
 
 const numberFormat = new Intl.NumberFormat('en-US');
 
