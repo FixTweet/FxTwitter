d682b07fa5470071dd228581db8b117ca20b797a
dangered wolf <d@ngeredwolf.me>
2023-11-12 19:50:31 -0500

    Make API routes more lenient (maybe #483?)

--- src/realms/api/router.ts
+++ src/realms/api/router.ts
@@ -7,10 +7,14 @@ import { Constants } from '../../constants';
 export const api = new Hono();
 
 /* Current v1 API endpoints. Currently, these still go through the Twitter embed requests. API v2+ won't do this. */
-api.get('/status/:id/:language?', statusRequest);
-api.get('/status/:id/:language?/', statusRequest);
-api.get('/:handle/status/:id/:language?', statusRequest);
-api.get('/:handle/status/:id/:language?/', statusRequest);
+api.get('/status/:id', statusRequest);
+api.get('/status/:id/', statusRequest);
+api.get('/status/:id/:language', statusRequest);
+api.get('/status/:id/:language/', statusRequest);
+api.get('/:handle/status/:id', statusRequest);
+api.get('/:handle/status/:id/', statusRequest);
+api.get('/:handle/status/:id/:language', statusRequest);
+api.get('/:handle/status/:id/:language/', statusRequest);
 api.get('/robots.txt', async c => c.text(Strings.ROBOTS_TXT_API));
 
 api.get('/:handle', profileRequest);
