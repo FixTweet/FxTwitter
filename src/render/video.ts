a812b4c87fb31ec7431ddf483b9b0a1a54245837
dangered wolf <d@ngeredwolf.me>
2023-12-10 19:52:09 -0500

    Fix not passing video thumbnail with og:image

--- src/render/video.ts
+++ src/render/video.ts
@@ -58,6 +58,7 @@ export const renderVideo = (
     `<meta property="og:video:height" content="${video.height * sizeMultiplier}"/>`,
     `<meta property="og:video:width" content="${video.width * sizeMultiplier}"/>`,
     `<meta property="og:video:type" content="${video.format}"/>`,
+    `<meta property="og:image" content="${video.thumbnail_url}"/>`,
     `<meta property="twitter:image" content="0"/>`
   ];
 
