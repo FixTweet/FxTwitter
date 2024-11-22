d950b19f2c314dfb0d2996b1ede6d96730e5f02d
dangered wolf <d@ngeredwolf.me>
2024-11-22 15:33:39 -0800

    Fix longer bluesky threads and instant view

--- src/providers/bsky/conversation.ts
+++ src/providers/bsky/conversation.ts
@@ -25,7 +25,7 @@ const fetchThread = async (
 
 export const fetchBskyThread = async (post: string, author: string, processThread = false) => {
   console.log(`Fetching post ${post} by ${author}`);
-  const thread = await fetchThread(post, author, processThread ? 20 : 1);
+  const thread = await fetchThread(post, author, processThread ? 10 : 1);
   if (!thread) {
     return null;
   }
@@ -85,8 +85,28 @@ export const constructBlueskyThread = async (
     }
     bucket.push(thread.post);
     if (thread.replies) {
-      const replies = followReplyChain(thread);
-      bucket.push(...replies);
+      let threadPiece = thread;
+      const totalReplies = [];
+      let replies = followReplyChain(threadPiece);
+
+      while (replies.length > 8) {
+        // Load more replies
+        const id = replies[replies.length - 1].uri.match(/(?<=post\/)(\w*)/g)?.[0] ?? '';
+        console.log('Fetching next thread piece', id);
+        const _threadPiece = await fetchBskyThread(id, author, true);
+        if (!_threadPiece) {
+          break;
+        }
+        threadPiece = _threadPiece.thread;
+        const moreReplies = followReplyChain(threadPiece);
+        if (!moreReplies.length) {
+          break;
+        }
+        replies = moreReplies;
+        totalReplies.push(...replies);
+      }
+
+      bucket.push(...totalReplies);
     }
   } else {
     bucket.push(thread.post);
