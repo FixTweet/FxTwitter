7730817a8d1a94d6af84c60c9233aafe3e196c03
dangered wolf <d@ngeredwolf.me>
2025-03-20 15:03:35 -0700

    fix: tests for real for real

--- test/worker.test.ts
+++ test/worker.test.ts
@@ -6,7 +6,7 @@ const humanHeaders = {
   'User-Agent':
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 };
-const githubUrl = 'https://github.com/FixTweet/FxTwitter';
+const githubUrl = 'https://github.com/FxEmbed/FxEmbed';
 const twitterBaseUrl = 'https://x.com';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
